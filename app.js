faccc391f2106e847eec29b9b81e197a5436a005
Carlos Meneses <camenesess@fcc.es>
2015-07-02 19:26:58 +0200

    Layouts

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express 	 = require('express');
+var path 		 = require('path');
+var favicon 	 = require('serve-favicon');
+var logger 		 = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser	 = require('body-parser');
+var partials	 = require('express-partials');
 
 var routes = require('./routes/index');
 //var users = require('./routes/users');
@@ -14,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(partials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
