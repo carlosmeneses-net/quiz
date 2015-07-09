76a3f05af85684d42f9a922f1bd0a2487de46821
Carlos Meneses <git@carlosmeneses.net>
2015-07-09 23:57:14 +0200

    Modulo 9 - Tema 1

---
+++ models/comment.js
@@ -0,0 +1,12 @@
+//Definicion de Comment con validación
+
+module.exports = function(sequelize, DataTypes){
+  return sequelize.define(
+    'Comment',
+    { texto: {
+      type: DataTypes.STRING,
+      validate: { notEmpty: {msg: "-> Falta comentario"}}
+      }
+    }
+  );
+}
